373a03580fc7b3d86ac2726a770ed8ddb21bebbf
Amber Johnson <amber.johnson.smile@gmail.com>
2018-03-20 11:02:42 -0600

    Add calendar file

---
+++ Calendar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class Calendar extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <Text>Calendar page!</Text>
+      </View>
+    );
+  }
+}
