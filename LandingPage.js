4b96b9dedbaf1084b888ce8aff8fadba4d0b699d
Amber Johnson <amber.johnson.smile@gmail.com>
2018-03-20 21:43:52 -0600

    Add description to landing page

--- LandingPage.js
+++ LandingPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, Button, Image, StyleSheet, View } from 'react-native';
+import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 import NavBar from './NavBar';
@@ -10,7 +10,9 @@ export default class LandingPage extends Component {
       <View style={{flex: 1, backgroundColor: 'white'}}>
         <NavBar />
         <View style={styles.main}>
-          <Image source={require('./assets/sipster.png')} style={{width: 224, height: 136, marginBottom: 30}} />
+          <Image source={require('./assets/sipster.png')} style={{width: 224, height: 136, marginBottom: 30, marginTop: -30}} />
+          <Text style={{textAlign: 'center'}}>Track daily intake. Set goals.</Text>
+          <Text style={{textAlign: 'center', marginBottom: 30}}>Increase awareness, increase health.</Text>
           <Button
             onPress={Actions.calendar}
             title="get sippin'"
