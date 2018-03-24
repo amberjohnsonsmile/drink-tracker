dd0718e90297127e7133ba8425064e556bc0d835
Amber Johnson <amber.johnson.smile@gmail.com>
2018-03-24 17:24:08 -0600

    Fetch data and add to page

--- Calendar.js
+++ Calendar.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 import DrinkList from './DrinkList';
 import NavBar from './NavBar';
 
 export default class Calendar extends Component {
+
   render() {
     return (
       <View style={{flex: 1}}>
         <NavBar />
         <View style={styles.main}>
-          <Text style={{alignSelf: 'center', fontSize: 26, fontWeight: 'bold', color: 'lightgreen', paddingTop: 30}}>drink history</Text>
+          <Text
+            style={{
+              alignSelf: 'center',
+              fontSize: 26,
+              fontWeight: 'bold',
+              color: 'lightgreen',
+              paddingTop: 30
+            }}>
+            drink history
+          </Text>
           <DrinkList />
         </View>
       </View>
--- DrinkList.js
+++ DrinkList.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default class DrinkList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {isLoading: true};
+  }
+
+  componentDidMount() {
+    return fetch('http://sipster-tracker.herokuapp.com/drinks')
+      .then(response => response.json())
+      .then(response => {
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: response.drinks
+          },
+          function() {}
+        );
+      })
+      .catch(console.error);
+  }
+
   render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+    
     return(
       <View style={styles.container}>
         <FlatList
-          data={[
+          data={this.state.dataSource}
+          /*{[
             {key: 'Friday, March 2', drinks: 3},
             {key: 'Thursday, March 8', drinks: 1},
             {key: 'Friday, March 9', drinks: 3},
@@ -15,13 +46,14 @@ export default class DrinkList extends Component {
             {key: 'Friday, March 16', drinks: 3},
             {key: 'Saturday, March 17', drinks: 1},
             {key: 'Sunday, March 18', drinks: 1}
-          ]}
+          ]}*/
           renderItem={({item}) =>
             <View style={styles.itemContainer}>
-              <Text style={styles.date}>{item.key}:</Text>
+              <Text style={styles.date}>{item.date}:</Text>
               <Text style={styles.drinks}>{item.drinks}</Text>
             </View>
           }
+          keyExtractor={(item, index) => index}
         />
       </View>
     );
