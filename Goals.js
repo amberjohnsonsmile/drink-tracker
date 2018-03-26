5f77ca180600d68f179d7c0f51c3358cdd8957f3
Amber Johnson <amber.johnson.smile@gmail.com>
2018-03-25 21:11:01 -0600

    Add frontend code to fetch goals

--- Goals.js
+++ Goals.js
@@ -3,25 +3,45 @@ import {Image, StyleSheet, Text, View} from 'react-native';
 import NavBar from './NavBar';
 
 export default class Goals extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isLoading: true};
+  }
+
+  componentDidMount() {
+    return fetch('http://sipster-tracker.herokuapp.com/goals')
+      .then(response => response.json())
+      .then(response => {
+        this.setState(
+          {
+            isLoading: false,
+            goalsData: response.goals
+          },
+          function() {}
+        );
+      })
+      .catch(console.error);
+  }
+
   render() {
     return (
       <View style={{flex: 1}}>
         <NavBar />
         <View style={styles.main}>
           <Text style={styles.goals}>goals</Text>
-          <View style={{flexDirection: 'row'}}>
+          <View style={styles.listItem}>
             <Image
               source={require('./assets/drink.png')}
-              style={{width: 20, height: 30}}
+              style={{width: 36, height: 50}}
             />
-            <Text>Drink water between every drink</Text>
+            <Text style={styles.listText}>Drink water between every drink</Text>
           </View>
           <View style={{flexDirection: 'row'}}>
             <Image
               source={require('./assets/drink.png')}
-              style={{width: 20, height: 30}}
+              style={{width: 36, height: 50}}
             />
-            <Text>Drink water between every drink</Text>
+            <Text style={styles.listText}>Eat before drinking</Text>
           </View>
         </View>
       </View>
@@ -42,5 +62,13 @@ const styles = StyleSheet.create({
     color: 'lightgreen',
     paddingTop: 40,
     paddingBottom: 40
+  },
+  listItem: {
+    flexDirection: 'row',
+    padding: 20
+  },
+  listText: {
+    fontSize: 16,
+    padding: 10
   }
 });
