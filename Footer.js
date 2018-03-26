8a7321668581be3d66dff9cb73653d10d8db2397
Amber Johnson <amber.johnson.smile@gmail.com>
2018-03-26 17:00:08 -0600

    Add styling to footer

--- Footer.js
+++ Footer.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 export default class Footer extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>&copy;2018 Amber Johnson</Text>
       </View>
     );
